6bb9246d92b585b8b6bd0bcb28e93c331a8bdb49
Angel K <282351876+angellllkr-eng@users.noreply.github.com>
2026-06-01 15:16:36 +0300

    Update and rename index to index.js
    Signed-off-by: Angel K <282351876+angellllkr-eng@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en" class="scroll-smooth">
 <head>
+  npm install
+  npm run 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MindReply | Executive Communication Intelligence Ecosystem</title>
