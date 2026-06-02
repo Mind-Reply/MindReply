db459cbe0043d71a4c3e59982af38cef6057f486
Angel K <282351876+angellllkr-eng@users.noreply.github.com>
2026-06-02 12:04:30 +0300

    Update index.js
    Signed-off-by: Angel K <282351876+angellllkr-eng@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-console.log("MindReply Node server is running");
 
-import http from "http";
-nmp run build
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("MindReply Node.js server is working");
-});
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
